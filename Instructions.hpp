019d070b40a698cbc3967804cb3fedd21de07c7d
llanesdavid97 <llanesdavid97@gmail.com>
2022-11-04 23:56:23 -0600

    1.0.0 version

---
+++ Instructions.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+class Instructions
+{
+    public:
+        void createVM();
+        void memorySettingUpVM();
+        void createDiskWithISO();
+        void enableRDP();
+        void startVM();
+};
